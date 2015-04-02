50418c5cd825e8e8f143979a128c7e72be90e54d
Eli <dgerena87d@gmail.com>
2015-04-02 13:09:18 -0400

    a lot  of updating to see what is breaking and why my app is crashing… I may have not removed enough to format over the html to ejs yet.

--- shell/assets/js/main.js
+++ shell/assets/js/main.js
@@ -1,10 +1,24 @@
-var container = document.querySelector('#container');
-// init
-var iso = new Isotope( container, {
-  // options
-  itemSelector: '.item',
-  layoutMode: 'fitRows'
-});
-var iso = new Isotope( '#container', {
-  // options
-});
+
+// var container = document.querySelector('#container');
+// // init
+// var iso = new Isotope( container, {
+//   itemSelector: '.item',
+//   getSortData: {
+//     name: '.name',
+//     category: '[data-category]'
+//   },
+//   masonry: {
+//     columnWidth: 200
+//   }
+// });
+(function($){
+	$('#container').isotope({
+	  // main isotope options
+	  itemSelector: '.item',
+	  layoutMode: 'masonry',
+	  masonry: {
+	    columnWidth: 200,
+	    rowHeight: 200
+	  }
+	});
+})(jQuery);
