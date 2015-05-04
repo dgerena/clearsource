4480760f7c8a744583e3b2d9074467e404b5685d
Eli <dgerena87d@gmail.com>
2015-05-03 23:10:14 -0400

    Gettting data figured out
    Had to figure out how i would structure the data and how the sessions
    would work. Had a heck of a time getting the user methods for sequelize
    through my thick head. Finally have that going and data going back and
    forth and into the view after also wrestling with ejs a bit.

--- shell/controllers/user.js
+++ shell/controllers/user.js
@@ -2,7 +2,7 @@ var route = require('express').Router();
 //To allow functions to run async
 var async = require('async');
 //For the password
-//var uuid = require('uuid');
+var uuid = require('uuid');
 
 route.get('/login', function (req,res){
 	console.log('user.js app get login');
@@ -10,7 +10,21 @@ route.get('/login', function (req,res){
 });
 
 route.post('/login', function (req,res){
-	console.log('login');
+	app.models.User
+		.find({
+			'where': {'email': req.body.email}
+		}).done(function (user) {
+			if (req.body.password == user.password) {
+				req.session.user = user;
+				console.log('working');
+				req.session.success = "You have signed in successfuly"
+				res.render('public/landing');
+			}else{
+				console.log('failed',req.body);
+				req.session.error = "You have failed to login, please check your email and password."
+				res.render('public/landing');
+			}
+		});
 });
 
 route.get('/signup',function (req,res){
@@ -19,17 +33,20 @@ route.get('/signup',function (req,res){
 });
 
 route.post('/signup',function (req,res){
-	console.log('user.js signup post',22);
-	app.models.Users
+	console.log('user.js signup post',22,req.body);
+	app.models.User
 		.findOrCreate({
-			'where':{ 'fullname':req.body.name},
+			'where':{ 'name':req.body.name},
 			'id':uuid.v4(),
-			'active':true,
-			'fullname':req.body.name,
-			'password':req.body.password
-		}).success(function(user){
-			res.json(user);
+			'active': true,
+			'name': req.body.name,
+			'password': req.body.password,
+			'email': req.body.email
 		})
+		.done(function (user){
+			console.log('hello',user);
+			res.render('user/login')
+		});
 });
 
 route.get('/logout',function (req,res){
--- shell/models/Users.js
+++ shell/models/Users.js
@@ -5,8 +5,9 @@ var sha224 = require('js-sha256').sha224;
 app.models.User = db.site.define('user', {
 	'id': {'type': Sequelize.UUID, 'defaultValue': Sequelize.UUIDV4, 'primaryKey': true},
 	'active': Sequelize.BOOLEAN,
-	'fullName': Sequelize.TEXT,
-	'password': Sequelize.STRING(56)
+	'name': Sequelize.TEXT,
+	'password': Sequelize.STRING(56),
+	'email': Sequelize.STRING()
 });
 
 db.site.sync();
