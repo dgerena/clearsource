4f9b4b0a54f812fb9d7363ad00ef811d20efd846
Eli <dgerena87d@gmail.com>
2015-07-09 22:19:32 -0400

    Moved to Sails

--- shell/config/config.js
+++
@@ -1,9 +0,0 @@
-var os = require('os');
-
-// set port
-var config = {
-	'http': process.env.PORT || 8080,
-	'numWorkers': 1, //os.cpus().length,
-	'fileTypes': ['.js','.cljs','.coffee']
-}
-module.exports = config;
